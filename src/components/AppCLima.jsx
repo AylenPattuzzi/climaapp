2edd4de4374281290805d684354156a09b599471
Aylen Pattuzzi <aylen@aiphag.com>
2024-02-22 01:51:57 -0300

    ...

---
+++ src/components/AppCLima.jsx
@@ -0,0 +1,14 @@
+import Formulario from "./Formulario"
+
+const AppCLima = () => {
+  return (
+    <>
+      <main className="dos-columnas">
+        <Formulario />
+      </main>
+
+    </>
+  )
+}
+
+export default AppCLima
